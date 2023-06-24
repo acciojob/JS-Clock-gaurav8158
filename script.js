2854b16f5e7624fc3b9c873232e9da70c1e69879
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-06-25 00:39:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-
-
 //your code here
 function setClock() {
-    const hourHand = document.querySelector('.hour-hand');
-    const minuteHand = document.querySelector('.min-hand');
-    const secondHand = document.querySelector('.second-hand');
-  
-    const currentDate = new Date();
-    const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
-    const seconds = currentDate.getSeconds();
-    document.getElementById("time").innerHTML = `${hours%12}:${minutes}:${seconds}`;
+  const hourHand = document.querySelector('.hour-hand');
+  const minuteHand = document.querySelector('.min-hand');
+  const secondHand = document.querySelector('.second-hand');
 
-    const hourRotation = ((hours %12) * 30) + (minutes / 2)+90;
-    const minuteRotation = (minutes * 6) + (seconds / 10)+90;
-    const secondRotation = seconds * 6+90;
-  
-    hourHand.style.transform = `rotate(${hourRotation}deg)`;
-    minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
-    secondHand.style.transform = `rotate(${secondRotation}deg)`;
-  }
-  
-  setInterval(setClock, 1000); // Update the clock every second
+  const currentDate = new Date();
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const seconds = currentDate.getSeconds();
+
+  const hourRotation = (hours * 30) + (minutes / 2);
+  const minuteRotation = (minutes * 6) + (seconds / 10);
+  const secondRotation = seconds * 6;
+
+  hourHand.style.transform = `rotate(${hourRotation}deg)`;
+  minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
+  secondHand.style.transform = `rotate(${secondRotation}deg)`;
+}
+
+setInterval(setClock, 1000); // Update the clock every second
